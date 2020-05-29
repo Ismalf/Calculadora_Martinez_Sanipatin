65577e1b9a59746960d5a9a8e5ecbb692e14d921
Ismael <ismalfmp@gmail.com>
2020-05-29 16:57:43 -0500

    calculator input ready

--- app/src/main/java/com/example/calculadora_martinez_sanipatin/MainActivity.java
+++ app/src/main/java/com/example/calculadora_martinez_sanipatin/MainActivity.java
@@ -25,23 +25,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void addToInput(View k){
         switch(k.getId()){
-            case (R.id.number1):  this._input+="1"; input.setText(_input);
-            case (R.id.number2):  this._input+="2"; input.setText(_input);
-            case (R.id.number3):  this._input+="3"; input.setText(_input);
-            case (R.id.number4):  this._input+="4"; input.setText(_input);
-            case (R.id.number5):  this._input+="5"; input.setText(_input);
-            case (R.id.number6):  this._input+="6"; input.setText(_input);
-            case (R.id.number7):  this._input+="7"; input.setText(_input);
-            case (R.id.number8):  this._input+="8"; input.setText(_input);
-            case (R.id.number9):  this._input+="9"; input.setText(_input);
-            case (R.id.number0):  this._input+="0"; input.setText(_input);
-            case (R.id.parenthesisopen):  this._input+="("; input.setText(_input);
-            case (R.id.parenthesisclose):  this._input+=")"; input.setText(_input);
-            case (R.id.divide):  this._input+="/"; input.setText(_input);
-            case (R.id.decimal):  this._input+="."; input.setText(_input);
-            case (R.id.addition):  this._input+="+"; input.setText(_input);
-            case (R.id.substract):  this._input+="-"; input.setText(_input);
-            case (R.id.multi):  this._input+="x"; input.setText(_input);
+            case (R.id.number1):  this._input+="1"; input.setText(_input); break;
+            case (R.id.number2):  this._input+="2"; input.setText(_input); break;
+            case (R.id.number3):  this._input+="3"; input.setText(_input); break;
+            case (R.id.number4):  this._input+="4"; input.setText(_input); break;
+            case (R.id.number5):  this._input+="5"; input.setText(_input); break;
+            case (R.id.number6):  this._input+="6"; input.setText(_input); break;
+            case (R.id.number7):  this._input+="7"; input.setText(_input); break;
+            case (R.id.number8):  this._input+="8"; input.setText(_input); break;
+            case (R.id.number9):  this._input+="9"; input.setText(_input); break;
+            case (R.id.number0):  this._input+="0"; input.setText(_input); break;
+            case (R.id.parenthesisopen):  this._input+="("; input.setText(_input); break;
+            case (R.id.parenthesisclose):  this._input+=")"; input.setText(_input);break;
+            case (R.id.divide):  this._input+="/"; input.setText(_input);break;
+            case (R.id.decimal):  this._input+="."; input.setText(_input);break;
+            case (R.id.addition):  this._input+="+"; input.setText(_input);break;
+            case (R.id.substract):  this._input+="-"; input.setText(_input);break;
+            case (R.id.multi):  this._input+="x"; input.setText(_input);break;
 
         }
     }
@@ -49,10 +49,13 @@ public class MainActivity extends AppCompatActivity {
     public void clearInput(View v){
         _input = "";
         input.setText(_input);
+        _result="";
+        result.setText(_result);
     }
 
-    public void backspace(View v){
-        _input = _input.substring(0,_input.length()-2);
+    public void removeLast(View v){
+        _input = _input.substring(0,_input.length()-1);
+        input.setText(_input);
     }
 
     public void calculate(View v){
